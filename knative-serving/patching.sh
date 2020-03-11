49b459c0e9e18d8ff3ad77e21160370cc9647581
gdambor <glenioborges@gmail.com>
2020-03-11 13:17:46 -0400

    Knative tutorial

---
+++ knative-serving/patching.sh
@@ -0,0 +1,4 @@
+kubectl patch configmap/config-network \
+      --namespace knative-serving \
+      --type merge \
+      --patch '{"data":{"ingress.class":"contour.ingress.networking.knative.dev"}}'
